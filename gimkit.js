954d16474aac6859d40185a03a9611c2118737f2
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-05-12 10:17:47 -0600

    Update gimkit.js

--- gimkit.js
+++ gimkit.js
@@ -5,18 +5,15 @@ chrome.storage.sync.get({
 }, (items) => {
     if (items.gimkitTranslate) {
         const target = document.getRootNode()
-        const callback = () => {
-            const text = document.getElementsByClassName("notranslate sc-gDPesD dUMQFj")
-            if (text.length < 1) {
-                console.log("Could not find element :/")
-            } else if (text.length > 1) {
-                console.log("Found too many elements :/")
-            } else {
-                console.log(text[0].innerHTML)
-            }
-        }
+        const cachedAnswers = {}
+        
+        const observer =  new MutationObserver(
+              () => {
+                const question = document.querySelector(".sc-kRCAcj > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1)").innerText
+                const answerBoxes = document.querySelector(".sc-cBXKeB").children
 
-        const observer = new MutationObserver(callback)
+              }
+            ).observe(document, {childList: true, subtree: true })
 
         observer.observe(target, {
             attributes: true,
